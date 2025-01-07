40706c0784206e4ce5dc14e68280203aaad6affe
DongHyunKim <rkrkwnj1046@gmail.com>
2025-01-07 19:39:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 28.12 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,39 +1,13 @@
-import java.util.*;
 class Solution {
     public String solution(int[] food) {
-        return foodForParticipantB(foodForParticipantA(food));
-    }
+        String answer = "0";
 
-    public List<Integer> foodForParticipantA(int[] foods) {
-        List<Integer> foodA = new ArrayList<>();
-        int foodOrder = 1;
-
-        for (int i = 1; i < foods.length; i++) {
-            if (2 <= foods[i]) {
-                for (int j = 0; j < (int) Math.floor((double) foods[i] / 2); j++) {
-                    foodA.add(foodOrder);
-                }
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
-            foodOrder++;
         }
-        return foodA;
-    }
 
-    public String foodForParticipantB(List<Integer> foodA) {
-        StringBuilder totalFood = new StringBuilder();
-
-        //왼쪽 음식 세팅
-        for (Integer eachFood : foodA) {
-            totalFood.append(eachFood);
-        }
-        //중간에 물 넣기
-        totalFood.append("0");
-
-        //오른쪽 음식 세팅
-        Collections.reverse(foodA);
-        for (Integer eachFood : foodA) {
-            totalFood.append(eachFood);
-        }
-        return totalFood.toString();
+        return answer;
     }
 }
