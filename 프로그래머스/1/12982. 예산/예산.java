eb222d6f075e1403607c02653a6d881d7a5304df
DongHyunKim <rkrkwnj1046@gmail.com>
2025-01-13 16:50:31 +0900

    [level 1] Title: 예산, Time: 1.25 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,16 +1,9 @@
+import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
         //지원요청비 오름차순 정령
-        for (int i = 0; i < d.length; i++) {
-            for (int j = i + 1; j < d.length; j++) {
-                if (d[i] > d[j]) {
-                    int temp = d[i];
-                    d[i] = d[j];
-                    d[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(d);
         //예산 - 요청 지원비
         for (int i = 0; i < d.length; i++) {
             if (budget >= d[i]){
