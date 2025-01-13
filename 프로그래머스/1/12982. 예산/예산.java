95d80e0520770608e283c13c8900c7b8f7ed3202
DongHyunKim <rkrkwnj1046@gmail.com>
2025-01-13 21:05:18 +0900

    [level 1] Title: 예산, Time: 0.28 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,9 +1,17 @@
-import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        //지원요청비 오름차순 정령
-        Arrays.sort(d);
+        //지원요청비 오름차순 정렬 => Arrays.sort(d)로도 가능한 문제 (속도 1.25ms)
+        for (int i = 0; i < d.length; i++) {
+            for (int j = i + 1; j < d.length; j++) {
+                if (d[i] > d[j]) {
+                    int temp = d[i];
+                    d[i] = d[j];
+                    d[j] = temp;
+                }
+            }
+        }
+        
         //예산 - 요청 지원비
         for (int i = 0; i < d.length; i++) {
             if (budget >= d[i]){
