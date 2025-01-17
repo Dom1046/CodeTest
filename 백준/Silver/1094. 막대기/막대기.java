4f43978bddfa8cf53d7d9bd4d822eef4e6058b0d
DongHyunKim <rkrkwnj1046@gmail.com>
2025-01-17 20:53:47 +0900

    [Silver V] Title: 막대기, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -10,48 +10,42 @@ public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int X = scanner.nextInt();
-        // X보다 작게 만들기 (poll <= X)
+        //1차 막대 크기 X보다 작게하기
         makeSmaller(X);
-        // 재귀를 이용해 남은 길이만큼 필요한지 체크하며 자르고 붙이기
-        polls.add(poll); // 처음 poll은 이미 X보다 작거나 같으므로 일단 붙여둔다
+        //자른 막대 한번 넣어주기
+        polls.add(poll);
+        //X와 붙인 막대기 크기 비교하며, 계속 자르기
         makeSmaller2(X);
-        // 붙인 막대 수 출력
+        // 막대기 수 반환
         System.out.println(polls.size());
     }
 
-    // 막대를 절반으로 자르기
-    public static int cutHalf(int poll) {
-        return poll / 2;
+    //막대 절반으로 자르기
+    public static int cutHalf(int poll){
+        return poll/2;
     }
-
-    // 지금까지 붙인 막대들의 합을 구하기
-    public static int attachPolls() {
+    //막대 붙여보기
+    public static int attachPolls(){
+        //막대 모두 붙이기
         return polls.stream().reduce(0, Integer::sum);
     }
-
-    // poll을 X보다 작거나 같게 만들기
-    public static void makeSmaller(int X) {
-        // poll이 X보다 클 동안 계속 절반으로 자른다
-        if (poll > X) {
+    //막대 사이즈 작게 만들기
+    public static void makeSmaller(int X){
+        if (poll > X){
             poll = cutHalf(poll);
-            makeSmaller(X);
-        } 
-        // poll <= X면 종료
+        }else return;
+        makeSmaller(X);
     }
-
-    // 재귀적으로 남은 길이를 확인하며 poll을 자르고, 필요하면 붙이는 로직
-    public static void makeSmaller2(int X) {
-        // base condition: 이미 X만큼 붙였으면 종료
+    //막대 사이즈 작게 만들기
+    public static void makeSmaller2(int X){
+        //X와 길이가 같으면 나가기
         if (attachPolls() == X) {
             return;
         }
-
-        // poll을 절반으로 자른다
+        // 막대 자르기
         poll = cutHalf(poll);
-        // 지금까지 붙인 막대 합
-        int sum = attachPolls();
-        // 필요한 나머지 길이
-        int rest = X - sum;
+        // X - 붙인 막대길이
+        int rest = X - attachPolls();
 
         // 만약 rest가 현재 poll보다 크거나 같다면 이 poll도 붙여야 함
         if (rest >= poll) {
