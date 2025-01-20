6924c2847ae38194bf1c419d6570fd933a17ba4a
DongHyunKim <rkrkwnj1046@gmail.com>
2025-01-20 23:33:39 +0900

    [Bronze II] Title: 하얀 칸, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] chessBoard = new String[8];
+        int index = 0;
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            chessBoard[index] = line;
+            index++;
+            if (index == 8){
+                break;
+            }
+        }
+        int cnt = 0;
+
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = (i%2==0)? 0:1; j < chessBoard.length; j +=2) {
+                char whiteBlock = chessBoard[i].charAt(j);
+                if (whiteBlock == 'F'){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
