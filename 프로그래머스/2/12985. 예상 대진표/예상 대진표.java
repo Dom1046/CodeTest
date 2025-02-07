04c3c07fd94ac7758ced802b26ea0267c58c035c
DongHyunKim <rkrkwnj1046@gmail.com>
2025-02-07 20:48:18 +0900

    [level 2] Title: 예상 대진표, Time: 225.49 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -7,33 +7,47 @@ class Solution{
             return 0;
         }
         int answer = 0;
+        int index = 0;
         List<Integer> list = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
 
-        while (list.size() > 1) {
+        // 2^n n값 구하기
+        int m = getSquare(n);
+
+
+        for (int i = 0; i < m; i++) {
             List<Integer> nextRound = new ArrayList<>();
-            for (int i = 0; i < list.size(); i += 2) {
-                int p1 = list.get(i);
-                int p2 = list.get(i + 1);
-
-                if ((p1 == a && p2 == b) || (p1 == b && p2 == a)) {
-                    return answer + 1;
+            for (int j = 0; j < list.size()-1; j += 2) {
+                int p1 = list.get(j);
+                int p2 = list.get(j + 1);
+                if ((list.get(j) == a || list.get(j) == b) &&
+                        (list.get(j + 1) == a || list.get(j + 1) == b)) {
+                    index++;
+                    break;
                 }
 
-                if (p1 == a || p1 == b) {
+                if (list.get(j) == a || list.get(j) == b) {
                     nextRound.add(p1);
-                } else if (p2 == a || p2 == b) {
+                } else if (list.get(j + 1) == a || list.get(j + 1) == b) {
                     nextRound.add(p2);
-                } else {
-                    nextRound.add(p1);
-                }
+                }else nextRound.add(p1);
             }
             list = nextRound;
+            if (index == 1) break;
             answer++;
         }
         return answer+1;
+    }
+
+    public int getSquare(int n) {
+        int cnt = 0;
+        while (n > 1) {
+            n /= 2;
+            cnt++;
+        }
+        return cnt;
     }
 }
