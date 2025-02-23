4f6b8835804e930497b62c91ae952dad4553c50c
DongHyunKim <rkrkwnj1046@gmail.com>
2025-02-23 23:08:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -9,22 +9,20 @@ public class Main {
         for (int i = 0; i < T; i++) {
             int n = sc.nextInt();
 
-            int answer = 0;
             if (n > 11) {
                 System.out.println("n이 11보다 큽니다.");
                 break;
             }
-           if (n == 1) answer = 1;
-           else if (n == 2) answer = 2;
-           else if (n == 3) answer = 4;
-           else if (n == 4) answer = 7;
-           else if (n == 5) answer = 13;
-           else if (n == 6) answer = 24;
-           else if (n == 7) answer = 44;
-           else if (n == 8) answer = 81;
-           else if (n == 9) answer = 149;
-           else if (n == 10) answer = 274;
-           System.out.println(answer);
+            int[] dp = new int[11];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+            System.out.println(dp[n]);
         }
     }
 }
