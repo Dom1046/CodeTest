eb4c0c1331392eee0dfea2f1800e5c0de162fe79
DongHyunKim <rkrkwnj1046@gmail.com>
2025-03-06 20:38:33 +0900

    [Bronze II] Title: 막대기, Time: 792 ms, Memory: 113888 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//막대기
+public class Main {
+
+    public static void getMaxCnt(int[] arr) {
+        int max = arr[arr.length - 1];
+        int cnt = 1;
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i - 1] > max) {
+                max = arr[i - 1];
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        getMaxCnt(arr);
+    }
+}
