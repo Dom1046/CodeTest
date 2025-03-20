79c8da5f2eea43ff432b3a6ba43d875ca95e29c5
DongHyunKim <rkrkwnj1046@gmail.com>
2025-03-21 00:02:44 +0900

    [Gold III] Title: 내리막 길, Time: 1048 ms, Memory: 214944 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static int m, n;
+    private static int[][] map;
+    private static int[][] dp;
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        m = sc.nextInt();
+        n = sc.nextInt();
+
+        map = new int[m + 1][n + 1];
+        dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = sc.nextInt();
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(1, 1));
+    }
+
+    private static int dfs(int x, int y) {
+        if (x == m && y == n) {
+            return 1;
+        }
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        } else {
+            dp[x][y] = 0;  
+            for (int i = 0; i < dx.length; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 1 || ny < 1 || nx > m || ny > n) {
+                    continue;
+                }
+
+                if (map[x][y] > map[nx][ny]) {
+                    dp[x][y] += dfs(nx, ny);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+}
