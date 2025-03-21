4d19117f82d9025c4cb3ea40b34f4ce5e76de050
DongHyunKim <rkrkwnj1046@gmail.com>
2025-03-21 22:17:23 +0900

    [Gold III] Title: 내리막 길, Time: 356 ms, Memory: 36476 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,26 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int m, n;
-    private static int[][] map;
-    private static int[][] dp;
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+    static int[][] map;
+    static int[][] dp;
 
-    public static void main(String[] args) {
+    static int m;
+    static int n;
 
-        Scanner sc = new Scanner(System.in);
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-        m = sc.nextInt();
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
         map = new int[m + 1][n + 1];
         dp = new int[m + 1][n + 1];
 
         for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                map[i][j] = sc.nextInt();
+                map[i][j] = Integer.parseInt(st.nextToken());
                 dp[i][j] = -1;
             }
         }
@@ -35,20 +42,20 @@ public class Main {
         if (dp[x][y] != -1) {
             return dp[x][y];
         } else {
-            dp[x][y] = 0;  
+            dp[x][y] = 0;
             for (int i = 0; i < dx.length; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx < 1 || ny < 1 || nx > m || ny > n) {
+                if (nx > m || ny > n || nx < 1 || ny < 1) {
                     continue;
                 }
-
-                if (map[x][y] > map[nx][ny]) {
+                if (map[nx][ny] < map[x][y]) {
                     dp[x][y] += dfs(nx, ny);
                 }
             }
         }
+
         return dp[x][y];
     }
 }
